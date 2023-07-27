e2dc6b6d028ff6eabe7ec7d4251bf5e2c9f58c5b
EcoFreshKase <alexander.jablo@gmail.com>
2023-07-27 15:56:22 +0200

    changed the font size of path labels

--- src/gui_builder/file_path.rs
+++ src/gui_builder/file_path.rs
@@ -4,17 +4,25 @@ and the path to which the file gets saved
 */
 
 use druid::{Widget,Insets, UnitPoint, Env};
-use druid::widget::{Label, WidgetExt, TextBox, Padding, Flex};
+use druid::widget::{Label, WidgetExt, TextBox, Padding, Flex, LineBreaking};
+use druid::text::FontDescriptor;
 
 use crate::gui_builder::AppState::AppState;
 
 pub fn file_path_builder() -> impl Widget<AppState> {
+    let path_font =FontDescriptor::new(druid::piet::FontFamily::SYSTEM_UI)
+        .with_size(13.5);
+
     let file_text = Label::new("File-Path:");
     
     let file_path_label = Label::new(|data: &AppState, _env: &Env| { //create file_path_label
         format!("{}", data.file_path)
-    }).align_left();
-    let file_name_text_box = TextBox::new() //field to input the name of the file to save
+    }).with_font(path_font.clone())
+    .with_line_break_mode(LineBreaking::WordWrap)
+    .align_left();
+    let file_name_text_box = TextBox::multiline() //field to input the name of the file to save
+        .with_font(path_font)
+        .with_line_wrapping(true)
         .lens(AppState::save_path);
     let file_path_container = Padding::new(Insets { //padding widget
         x0: 10.0,
