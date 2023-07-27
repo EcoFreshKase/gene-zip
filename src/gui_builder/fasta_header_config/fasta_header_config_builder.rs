2edfc42f7dd0a494709c281a935aa23b0084cfb5
EcoFreshKase <alexander.jablo@gmail.com>
2023-07-28 01:12:18 +0200

    removed unnecessary code

--- src/gui_builder/fasta_header_config/fasta_header_config_builder.rs
+++ src/gui_builder/fasta_header_config/fasta_header_config_builder.rs
@@ -7,11 +7,6 @@ use druid::widget::{Flex, Label, Button};
 use super::config_sub_builder::sub_window_builder;
 use super::super::AppState::AppState;
 
-#[derive(Data, Lens, Clone)]
-struct TestData {
-    string: &'static str,
-}
-
 pub fn config_builder() -> impl Widget<AppState> {
     let config_label = Label::new("fasta-header configuration:");
     let config_button = Button::new("config header")//button to create sub window
