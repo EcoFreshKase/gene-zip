c3a57b33c76e6028f26d2c0d38663f0af5eb35d1
EcoFreshKase <alexander.jablo@gmail.com>
2023-08-05 14:18:59 +0200

    Changed visualization of Save Path and File Path

--- src/gui_builder/file_path.rs
+++ src/gui_builder/file_path.rs
@@ -13,29 +13,32 @@ pub fn file_path_builder() -> impl Widget<AppState> {
     let path_font =FontDescriptor::new(druid::piet::FontFamily::SYSTEM_UI)
         .with_size(13.5);
 
-    let file_text = Label::new("File-Path:");
-    
+    let padding = (0.0, 3.0);
+
+    let current_path_label = Label::new("Current Path:");
     let file_path_label = Label::new(|data: &AppState, _env: &Env| { //create file_path_label
         format!("{}", data.file_path)
     }).with_font(path_font.clone())
     .with_line_break_mode(LineBreaking::WordWrap)
     .align_left();
+    let current_path_container = Flex::row() // Contains all Widgets showing the current selected path
+        .with_child(current_path_label)
+        .with_child(file_path_label)
+        .padding(padding);
+
+    let save_path_label = Label::new("Save Path:");
     let file_name_text_box = TextBox::multiline() //field to input the name of the file to save
         .with_font(path_font)
         .with_line_wrapping(true)
         .lens(AppState::save_path);
-    let file_path_container = Padding::new(Insets { //padding widget
-        x0: 10.0,
-        y0: 0.0,
-        x1: 0.0,
-        y1: 0.0,
-    }, Flex::column()
-        .with_child(file_path_label)
-        .with_child(file_name_text_box));
+    let save_path_container = Flex::row()
+        .with_child(save_path_label)
+        .with_child(file_name_text_box)
+        .padding(padding);
 
-    Flex::row()
-        .with_child(file_text)
-        .with_child(file_path_container)
+    Flex::column()
+        .with_child(current_path_container)
+        .with_child(save_path_container)
         .align_vertical(UnitPoint::TOP_LEFT)
         .align_left()
 }
